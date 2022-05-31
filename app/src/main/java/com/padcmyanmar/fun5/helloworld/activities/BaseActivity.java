c7c5573b884a03ce2cf107707f124fcf37f635a4
Wai Yan Myint Myat <T964277@wavemoney.com.mm>
2022-05-31 15:52:05 +0630

    change name at BaseActivity.java

--- app/src/main/java/com/padcmyanmar/fun5/helloworld/activities/BaseActivity.java
+++ app/src/main/java/com/padcmyanmar/fun5/helloworld/activities/BaseActivity.java
@@ -3,7 +3,7 @@ package com.padcmyanmar.fun5.helloworld.activities;
 import android.support.v7.app.AppCompatActivity;
 
 /**
- * Created by Phyo Thiha on 6/3/18.
+ * Created by Wai Yan Myint Myat on 6/3/18.
  */
 public abstract class BaseActivity extends AppCompatActivity {
 
